87d895362f7ffd064aa6b32762879bc564610e06
Timo Vahtera <timo.vahtera@solita.fi>
2021-12-01 10:29:17 +0200

    Too legible, worsen legibility!

--- day01.fsx
+++ day01.fsx
@@ -5,15 +5,7 @@ let input = System.IO.File.ReadAllLines "inputs/day01.txt" |> Seq.map int
 let deeper (x,y) = x < y 
 
 // Answer 1
-Seq.pairwise input
-|> Seq.where deeper
-|> Seq.length
-|> printfn "%i"
+Seq.pairwise input |> Seq.where deeper |> Seq.length |> printfn "%i"
 
 // Answer 2
-Seq.windowed 3 input
-|> Seq.map Seq.sum
-|> Seq.pairwise
-|> Seq.where deeper
-|> Seq.length
-|> printfn "%i"
+Seq.windowed 3 input |> Seq.map Seq.sum |> Seq.pairwise |> Seq.where deeper |> Seq.length |> printfn "%i"
