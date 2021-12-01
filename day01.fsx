752bc3880465095ed7d230f571922916d9bfb9ab
Timo Vahtera <timo.vahtera@solita.fi>
2021-12-01 10:27:11 +0200

    Improve day 1 formatting and legibility

--- day01.fsx
+++ day01.fsx
@@ -2,9 +2,18 @@
 #time
 
 let input = System.IO.File.ReadAllLines "inputs/day01.txt" |> Seq.map int
+let deeper (x,y) = x < y 
 
 // Answer 1
-Seq.pairwise input |> Seq.filter (fun (x,y) -> x < y) |> Seq.length |> printfn "%i"
+Seq.pairwise input
+|> Seq.where deeper
+|> Seq.length
+|> printfn "%i"
 
 // Answer 2
-Seq.windowed 3 input |> Seq.map Seq.sum |> Seq.pairwise |> Seq.filter (fun (x,y) -> x < y) |> Seq.length |> printfn "%i"
+Seq.windowed 3 input
+|> Seq.map Seq.sum
+|> Seq.pairwise
+|> Seq.where deeper
+|> Seq.length
+|> printfn "%i"
