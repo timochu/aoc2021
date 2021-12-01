3b9ea07e7bd245562d67e7319b6cc7980db6f42c
Timo Vahtera <timo.vahtera@solita.fi>
2021-12-01 10:44:54 +0200

    Day 1, improve naming of inputs variable

--- day01.fsx
+++ day01.fsx
@@ -1,11 +1,11 @@
 // puzzle: https://adventofcode.com/2021/day/1
 #time
 
-let input = "inputs/day01.txt" |> System.IO.File.ReadAllLines |> Seq.map int
+let depths = "inputs/day01.txt" |> System.IO.File.ReadAllLines |> Seq.map int
 let deeper (x,y) = x < y 
 
 // Answer 1
-input |> Seq.pairwise |> Seq.where deeper |> Seq.length |> printfn "%i"
+depths |> Seq.pairwise |> Seq.where deeper |> Seq.length |> printfn "%i"
 
 // Answer 2
-input |> Seq.windowed 3 |> Seq.map Seq.sum |> Seq.pairwise |> Seq.where deeper |> Seq.length |> printfn "%i"
+depths |> Seq.windowed 3 |> Seq.map Seq.sum |> Seq.pairwise |> Seq.where deeper |> Seq.length |> printfn "%i"
