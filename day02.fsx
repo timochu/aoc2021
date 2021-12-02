e307f2c108dd4777e293abb3d34b5e0da8cb9b61
Timo Vahtera <timo.vahtera@solita.fi>
2021-12-02 13:36:06 +0200

    Simplify day 2

--- day02.fsx
+++ day02.fsx
@@ -6,25 +6,22 @@ let toCommand (line: string) =
     
 let commands = "inputs/day02.txt" |> System.IO.File.ReadAllLines |> Seq.map toCommand
 
-let position =
-    Seq.fold (fun (distance, depth) (direction, amount) ->
-        match direction with
-        | "up"   -> distance, depth - amount
-        | "down" -> distance, depth + amount
-        | _      -> distance + amount, depth) (0,0)
-    >> fun (distance, depth) -> distance * depth
-
-let position2 =
-    Seq.fold (fun (distance, depth, aim) (direction, amount) ->
-        match direction with
-        | "up"   -> distance, depth, aim - amount
-        | "down" -> distance, depth, aim + amount
-        | _      -> distance + amount, depth + (amount * aim), aim) (0,0,0)
-    >> fun (distance, depth, _) -> distance * depth
-
 // Answer 1
-commands |> position |> printfn "%i"
+commands |> 
+Seq.fold (fun (distance, depth) (direction, amount) ->
+    match direction with
+    | "up"   -> distance, depth - amount
+    | "down" -> distance, depth + amount
+    | _      -> distance + amount, depth) (0,0)
+|> fun (distance, depth) -> distance * depth
+|> printfn "%i"
 
 // Answer 2
-commands |> position2 |> printfn "%i"
-
+commands |> 
+Seq.fold (fun (distance, depth, aim) (direction, amount) ->
+    match direction with
+    | "up"   -> distance, depth, aim - amount
+    | "down" -> distance, depth, aim + amount
+    | _      -> distance + amount, depth + (amount * aim), aim) (0,0,0)
+|> fun (distance, depth, _) -> distance * depth 
+|> printfn "%i"
