d83289723633bac24e361fd551b3e3afa914752e
Timo Vahtera <timo.vahtera@solita.fi>
2021-12-02 12:15:57 +0200

    Day 2 formatting cleanup

--- day02.fsx
+++ day02.fsx
@@ -4,26 +4,26 @@
 type Command =  { Direction: string; Amount: int }
 
 let toCommand (c: string) =
-    let parts = c.Split ' '
-    { Direction = parts.[0]
-      Amount = parts.[1] |> int }
+    let (direction, amount) = c.Split ' ' |> fun x -> (x.[0], x.[1])
+    { Direction = direction
+      Amount = amount |> int }
 
 let commands = "inputs/day02.txt" |> System.IO.File.ReadAllLines |> Seq.map toCommand
 
 let position commands =
     let folder (distance, depth) command = 
         match command.Direction with
-        | "up" ->  (distance, depth - command.Amount)
-        | "down" ->  (distance, depth + command.Amount)
-        | _ ->  (distance + command.Amount, depth)
+        | "up"   -> distance, depth - command.Amount
+        | "down" -> distance, depth + command.Amount
+        | _      -> distance + command.Amount, depth
     ((0,0), commands) ||> Seq.fold folder |> fun (distance, depth) -> distance * depth
 
 let position2 commands =
     let folder (distance, depth, aim) command =
         match command.Direction with
-        | "up" ->  (distance, depth, aim - command.Amount)
-        | "down" ->  (distance, depth, aim + command.Amount)
-        | _ ->  (distance + command.Amount, depth + (command.Amount * aim), aim)
+        | "up"   -> distance, depth, aim - command.Amount
+        | "down" -> distance, depth, aim + command.Amount
+        | _      -> distance + command.Amount, depth + (command.Amount * aim), aim
     ((0,0,0), commands) ||> Seq.fold folder |> fun (distance, depth, _) -> distance * depth
 
 // Answer 1
