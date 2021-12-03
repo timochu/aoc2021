483dc9463340c0c8a9a55068ff0656cfdcde5e77
Timo Vahtera <timo.vahtera@solita.fi>
2021-12-03 20:13:55 +0200

    Day 3 part 2 first version

--- day03.fsx
+++ day03.fsx
@@ -10,4 +10,23 @@ diagnostics
 |> System.String.Concat
 |> fun bits -> System.Convert.ToUInt16 (bits, 2)
 |> fun gamma -> gamma * (~~~gamma <<< 4 >>> 4)
-|> printfn "%i"
+|> printfn "answer 1: %i"
+
+// Answer 2
+let mostCommon i transposed = 
+    transposed |> Seq.item i |> Seq.countBy id |> Seq.sortByDescending fst |> Seq.maxBy snd |> fst
+
+let leastCommon i transposed  = 
+    transposed |> Seq.item i |> Seq.countBy id |> Seq.sortBy fst |> Seq.minBy snd |> fst
+
+let rec rating i comparator diagnostics =
+    match diagnostics |> Seq.length with
+    | 1 -> diagnostics |> Seq.head |> fun bits -> System.Convert.ToInt32 (bits, 2)
+    | _ -> 
+        let common = diagnostics |> Seq.transpose |> comparator i
+        diagnostics |> Seq.where (fun bits -> bits.[i] = common) |> rating (i+1) comparator
+
+let oxygen = diagnostics |> rating 0 mostCommon
+let co2 = diagnostics |> rating 0 leastCommon
+
+printfn "answer 2: %i" (oxygen * co2)
