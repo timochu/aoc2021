ca4ac6e9f60f57f3b38457bdf548fd65f6581b44
Timo Vahtera <timo.vahtera@solita.fi>
2021-12-03 11:14:44 +0200

    Improve day 3

--- day03.fsx
+++ day03.fsx
@@ -3,11 +3,13 @@
 
 let rotate d = d |> Seq.head |> Seq.mapi (fun i _ -> d |> Seq.map (Seq.item i))
 let mostFrequent = Seq.countBy id >> Seq.maxBy snd >> fst
-
-let diagnostics = "inputs/day03.txt" |> System.IO.File.ReadAllLines |> rotate
-
-let gamma = diagnostics |> Seq.map mostFrequent |> Seq.toArray |> System.String |> fun b -> System.Convert.ToUInt16 (b, 2)
-let epsilon = ~~~gamma <<< 4 >>> 4 
+let diagnostics = "inputs/day03.txt" |> System.IO.File.ReadAllLines 
 
 // Answer 1
-printfn "%i" (gamma * epsilon)
+diagnostics 
+|> rotate
+|> Seq.map mostFrequent 
+|> Seq.toArray |> System.String 
+|> (fun s -> (System.Convert.ToUInt16 (s, 2))) 
+|> (fun i -> i * (~~~i <<< 4 >>> 4))
+|> printfn "%i"
