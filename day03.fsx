aca34b8c030c49ff50bc15e0542aea3c3594e687
Timo Vahtera <timo.vahtera@solita.fi>
2021-12-03 12:22:34 +0200

    Day 3 wording

--- day03.fsx
+++ day03.fsx
@@ -8,6 +8,6 @@ diagnostics
 |> Seq.transpose
 |> Seq.map (Seq.countBy id >> Seq.maxBy snd >> fst) 
 |> Seq.toArray |> System.String 
-|> fun binary -> System.Convert.ToUInt16 (binary, 2)
+|> fun bits -> System.Convert.ToUInt16 (bits, 2)
 |> fun gamma -> gamma * (~~~gamma <<< 4 >>> 4)
 |> printfn "%i"
