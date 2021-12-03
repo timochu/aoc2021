f8df9147856e123b181b289598af7a6386580b8a
Timo Vahtera <timo.vahtera@solita.fi>
2021-12-03 12:20:28 +0200

    Day 3 formatting

--- day03.fsx
+++ day03.fsx
@@ -8,6 +8,6 @@ diagnostics
 |> Seq.transpose
 |> Seq.map (Seq.countBy id >> Seq.maxBy snd >> fst) 
 |> Seq.toArray |> System.String 
-|> (fun s -> (System.Convert.ToUInt16 (s, 2))) 
-|> (fun i -> i * (~~~i <<< 4 >>> 4))
+|> fun binary -> (System.Convert.ToUInt16 (binary, 2))
+|> fun gamma -> gamma * (~~~gamma <<< 4 >>> 4)
 |> printfn "%i"
