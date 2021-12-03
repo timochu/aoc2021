24e4ae580dedcc6bdfef08d41416706d0d936839
Timo Vahtera <timo.vahtera@solita.fi>
2021-12-03 11:26:22 +0200

    Day 3 tighten

--- day03.fsx
+++ day03.fsx
@@ -1,13 +1,12 @@
 // puzzle: https://adventofcode.com/2021/day/3
 #time
 
-let mostFrequent = Seq.countBy id >> Seq.maxBy snd >> fst
 let diagnostics = "inputs/day03.txt" |> System.IO.File.ReadAllLines 
 
 // Answer 1
 diagnostics 
 |> Seq.transpose
-|> Seq.map mostFrequent 
+|> Seq.map (Seq.countBy id >> Seq.maxBy snd >> fst) 
 |> Seq.toArray |> System.String 
 |> (fun s -> (System.Convert.ToUInt16 (s, 2))) 
 |> (fun i -> i * (~~~i <<< 4 >>> 4))
