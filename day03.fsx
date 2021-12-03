b8ca1c28a5a1ab3dcf314fb98b8feb915383ecfc
Timo Vahtera <timo.vahtera@solita.fi>
2021-12-03 11:25:31 +0200

    Day 3, use transpose

--- day03.fsx
+++ day03.fsx
@@ -1,13 +1,12 @@
 // puzzle: https://adventofcode.com/2021/day/3
 #time
 
-let rotate d = d |> Seq.head |> Seq.mapi (fun i _ -> d |> Seq.map (Seq.item i))
 let mostFrequent = Seq.countBy id >> Seq.maxBy snd >> fst
 let diagnostics = "inputs/day03.txt" |> System.IO.File.ReadAllLines 
 
 // Answer 1
 diagnostics 
-|> rotate
+|> Seq.transpose
 |> Seq.map mostFrequent 
 |> Seq.toArray |> System.String 
 |> (fun s -> (System.Convert.ToUInt16 (s, 2))) 
