ee5bd7455ae418aea50c0244fd545c59b6708dfb
Timo Vahtera <timo.vahtera@gmail.com>
2021-12-04 18:01:11 +0200

    Improve day 4 legibility

--- day04.fsx
+++ day04.fsx
@@ -1,8 +1,6 @@
 #time // puzzle: https://adventofcode.com/2021/day/4
 
-type Board = 
-    { All: int array
-      Lines: int array array }
+type Board =  { All: int array ; Lines: int array array }
 
 let toBoard (s : string) = 
     s.Split ([|' '; '\n'|], System.StringSplitOptions.RemoveEmptyEntries) 
@@ -16,18 +14,18 @@ let isWinner drawn board = board.Lines |> Array.exists (fun line -> line |> Arra
 
 let calculateScore drawn board = board.All |> Array.except drawn |> Array.sum |> ( * ) (Array.last drawn)
 
-let rec score iteration numbers scores (boards : Board array) =
+let rec score iteration scores numbers boards =
     match iteration = Array.length numbers with
     | true -> scores
     | false ->
         let drawn = numbers |> Array.take iteration
         let winners = boards |> Array.where (isWinner drawn)
+        let scores = scores |> Array.append (winners |> Array.map (calculateScore drawn))
         let remaining = boards |> Array.except winners
-        let newScores = winners |> Array.map (calculateScore drawn)
-        score (iteration+1) numbers (scores |> Array.append newScores) remaining
+        score (iteration+1) scores numbers remaining
 
 let boards = "inputs/day04.txt" |> System.IO.File.ReadAllText |> fun s -> s.Split "\n\n" |> Array.skip 1 |> Array.map toBoard
 let numbers = "inputs/day04.txt" |> System.IO.File.ReadLines |> Seq.head |> fun s -> s.Split ',' |> Array.map int
 
 // Answer 1 & 2
-score 5 numbers Array.empty boards |> fun scores -> printfn "%i\n%i" (Array.last scores) (Array.head scores)
+(numbers, boards) ||> score 0 Array.empty |> fun scores -> printfn "%i\n%i" (Array.last scores) (Array.head scores)
