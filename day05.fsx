555c753ccaacffd47977ef87169e46433a3c2d92
Timo Vahtera <timo.vahtera@gmail.com>
2021-12-05 14:56:47 +0200

    Day 5 simplify tuples

--- day05.fsx
+++ day05.fsx
@@ -3,17 +3,17 @@
 let toLine (s : string) =
     s.Split ([|" -> "; ","|],  System.StringSplitOptions.RemoveEmptyEntries  )
     |> Array.map int
-    |> fun p -> (p[0], p[1]) , (p[2], p[3])
+    |> fun p -> p[0], p[1], p[2], p[3]
 
-let isHorizontalOrVertical ((x1 , y1) , (x2 , y2)) = 
+let isHorizontalOrVertical (x1, y1, x2 , y2) = 
     x1 = x2 || y1 = y2
 
-let getAllPoints ((x1 , y1) , (x2 , y2)) =
+let getAllPoints (x1, y1, x2, y2) =
     let xIncrement = if x1>x2 then -1 else 1
     let yIncrement = if y1>y2 then -1 else 1
     let xs = [| x1 .. xIncrement .. x2 |]
     let ys = [| y1 .. yIncrement .. y2 |]
-    if isHorizontalOrVertical ((x1,y1),(x2,y2)) then
+    if isHorizontalOrVertical (x1, y1, x2, y2) then
         ys |> Array.allPairs xs
     else 
         [| 0 .. Array.length xs - 1 |] |> Array.map (fun i -> xs[i], ys[i])
