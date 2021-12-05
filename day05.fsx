9382ece4736b78428d208c465b370192d0aedd53
Timo Vahtera <timo.vahtera@gmail.com>
2021-12-05 14:32:12 +0200

    Day 5 part 2 version 1

--- day05.fsx
+++ day05.fsx
@@ -11,22 +11,34 @@ let isHorizontalOrVertical ((x1 , y1) , (x2 , y2)) =
     x1 = x2 || y1 = y2
 
 let getAllPoints ((x1 , y1) , (x2 , y2)) =
-    let (xUpper, xLower) = if x1>x2 then x1,x2 else x2,x1
-    let (yUpper, yLower) = if y1>y2 then y1,y2 else y2,y1
-    [|
+    match isHorizontalOrVertical ((x1,y1),(x2,y2)) with
+    | false ->
+        let xIncrement = if x1>x2 then -1 else 1
+        let yIncrement = if y1>y2 then -1 else 1
+        let xs = [| x1 .. xIncrement .. x2 |]
+        let ys = [| y1 .. yIncrement .. y2 |]
+        [| 0 .. Array.length xs - 1 |] |> Array.map (fun i -> xs[i], ys[i])
+    | true -> 
+        let (xUpper, xLower) = if x1>x2 then x1,x2 else x2,x1
+        let (yUpper, yLower) = if y1>y2 then y1,y2 else y2,y1
+        [|
         for x in xLower .. xUpper do
             for y in yLower .. yUpper ->
                 x, y
-    |]
+        |]
+
+let overlapping = 
+    Array.map getAllPoints
+    >> Array.concat
+    >> Array.countBy id
+    >> Array.where (fun (_, count) -> count > 1)
+    >> Array.length
+    >> printfn "%A"
 
 let lines = "inputs/day05.txt" |> System.IO.File.ReadAllLines |> Array.map toLine
 
 // // Answer 1
-lines
-|> Array.where isHorizontalOrVertical 
-|> Array.map getAllPoints
-|> Array.concat
-|> Array.countBy id
-|> Array.where (fun (_, count) -> count > 1)
-|> Array.length
-|> printfn "%A"
+lines |> Array.where isHorizontalOrVertical |> overlapping
+
+// // Answer 2
+lines |> overlapping
