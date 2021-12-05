815e68532099efb3119d42458717008b754d034a
Timo Vahtera <timo.vahtera@gmail.com>
2021-12-05 12:52:46 +0200

    Improve day 5

--- day05.fsx
+++ day05.fsx
@@ -7,22 +7,24 @@ let toLine (s : string) =
     |> Array.map int
     |> fun p -> (p[0], p[1]) , (p[2], p[3])
 
-let isHorizontalOrVertical ((x1 , y1) , (x2 , y2)) = x1 = x2 || y1 = y2
-    
+let isHorizontalOrVertical ((x1 , y1) , (x2 , y2)) = 
+    x1 = x2 || y1 = y2
+
 let getAllPoints ((x1 , y1) , (x2 , y2)) =
-    match x1 = x2, y1 = y2, y1 > y2, x1 > x2 with
-    | true, _, true, _  -> [| for i in y2 .. y1 -> (x1, i) |]
-    | true, _, false, _ -> [| for i in y1 .. y2 -> (x1, i) |]
-    | _, true, _, true  -> [| for i in x2 .. x1 -> (i, y1) |]
-    | _, true, _, false -> [| for i in x1 .. x2 -> (i, y1) |]
-    | _ -> failwith "there was a diagonal line amongst the lines!"
-    
-// Answer 1
-"inputs/day05.txt" 
-|> System.IO.File.ReadAllLines 
-|> Array.map toLine 
+    let (xUpper, xLower) = if x1>x2 then x1,x2 else x2,x1
+    let (yUpper, yLower) = if y1>y2 then y1,y2 else y2,y1
+    [|
+        for x in xLower .. xUpper do
+            for y in yLower .. yUpper ->
+                x, y
+    |]
+
+let lines = "inputs/day05.txt" |> System.IO.File.ReadAllLines |> Array.map toLine
+
+// // Answer 1
+lines
 |> Array.where isHorizontalOrVertical 
-|> Array.map getAllPoints 
+|> Array.map getAllPoints
 |> Array.concat
 |> Array.countBy id
 |> Array.where (fun (_, count) -> count > 1)
