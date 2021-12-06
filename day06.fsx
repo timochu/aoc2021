f62fe5329dfb65578a67410ce4fa87a810d5f015
Timo Vahtera <timo.vahtera@gmail.com>
2021-12-06 19:39:35 +0200

    Day 6 optimize and formatting

--- day06.fsx
+++ day06.fsx
@@ -1,7 +1,7 @@
 #time // puzzle: https://adventofcode.com/2021/day/6
 
 let n num = Seq.where ((=) num) >> Seq.length >> uint64
-let fish = "inputs/day06.txt" |> System.IO.File.ReadAllText |> fun (s:string) -> s.Split ',' |> Seq.map int |> fun x -> [| n 0 x ; n 1 x ; n 2 x ; n 3 x; n 4 x; n 5 x; n 6 x; n 7 x ; n 8 x |]
+let fish = "inputs/day06.txt" |> System.IO.File.ReadAllText |> fun (s:string) -> s.Split ',' |> fun x -> [| n "0" x; n "1" x; n "2" x; n "3" x; n "4" x; n "5" x; n "6" x; n "7" x; n "8" x |]
 
 let rec count days fish =
     if days = 0 then fish |> Seq.sum
