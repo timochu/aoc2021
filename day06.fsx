add8416d48a5e57b31599487cd55c8e61f68b9c3
Timo Vahtera <timo.vahtera@gmail.com>
2021-12-06 20:03:34 +0200

    Day 6 formatting

--- day06.fsx
+++ day06.fsx
@@ -1,16 +1,11 @@
 #time // puzzle: https://adventofcode.com/2021/day/6
 
-let fish = 
-    let count num = Seq.where ((=) num) >> Seq.length >> int64
-    "inputs/day06.txt" |> System.IO.File.ReadAllText |> fun (s:string) -> s.Split ',' 
-    |> fun x -> [ 0L; count "1" x; count "2" x; count "3" x; count "4" x; count "5" x; 0L; 0L; 0L ]
+let c num = Seq.where ((=) num) >> Seq.length >> int64
+let fish = "inputs/day06.txt" |> System.IO.File.ReadAllText |> fun (s:string) -> s.Split ',' |> fun f -> [ 0L; c "1" f; c "2" f; c "3" f; c "4" f; c "5" f; 0L; 0L; 0L ]
 
-let rec count days f =
+let rec count f days =
     if days = 0 then f |> Seq.sum
-    else count (days-1) [ f[1]; f[2]; f[3]; f[4]; f[5]; f[6]; f[7] + f[0]; f[8]; f[0] ]
+    else count [ f[1]; f[2]; f[3]; f[4]; f[5]; f[6]; f[7] + f[0]; f[8]; f[0] ] (days-1)
 
-// Answer 1
-fish |> count 80 |> printfn "%i"
-
-// Answer 2
-fish |> count 256 |> printfn "%i"
+// Answer 1 & 2
+[ 80; 256 ] |> Seq.iter (count fish >> printfn "%i")
