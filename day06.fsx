de592604f94f532ce1602569b45ff4d4fae5cdee
Timo Vahtera <timo.vahtera@gmail.com>
2021-12-06 19:46:39 +0200

    Day 6 cleanup

--- day06.fsx
+++ day06.fsx
@@ -1,11 +1,11 @@
 #time // puzzle: https://adventofcode.com/2021/day/6
 
-let n num = Seq.where ((=) num) >> Seq.length >> uint64
-let fish = "inputs/day06.txt" |> System.IO.File.ReadAllText |> fun (s:string) -> s.Split ',' |> fun x -> [| 0UL; n "1" x; n "2" x; n "3" x; n "4" x; n "5" x; 0UL; 0UL; 0UL |]
+let n num = Seq.where ((=) num) >> Seq.length >> int64
+let fish = "inputs/day06.txt" |> System.IO.File.ReadAllText |> fun (s:string) -> s.Split ',' |> fun x -> [| 0L; n "1" x; n "2" x; n "3" x; n "4" x; n "5" x; 0L; 0L; 0L |]
 
-let rec count days fish =
-    if days = 0 then fish |> Seq.sum
-    else count (days-1) [| fish[1]; fish[2]; fish[3]; fish[4]; fish[5]; fish[6]; fish[7] + fish[0]; fish[8]; fish[0] |]
+let rec count days f =
+    if days = 0 then f |> Seq.sum
+    else count (days-1) [| f[1]; f[2]; f[3]; f[4]; f[5]; f[6]; f[7] + f[0]; f[8]; f[0] |]
 
 // Answer 1
 fish |> count 80 |> printfn "%i"
