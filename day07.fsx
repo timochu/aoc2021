b7bc49520861ef166b70e37a214253fd7cdf96f9
Timo Vahtera <timo.vahtera@solita.fi>
2021-12-07 08:42:08 +0200

    Day 7 part 2

--- day07.fsx
+++ day07.fsx
@@ -7,3 +7,9 @@ let positions = "inputs/day07.txt" |> System.IO.File.ReadAllText |> fun (s:strin
 |> Seq.map (fun r -> positions |> Seq.map(fun pos -> r-pos |> abs) |> Seq.sum)  
 |> Seq.min
 |> printfn "%A"
+
+// Answer 2
+[ positions |> Seq.min .. positions |> Seq.max ]
+|> Seq.map (fun r -> positions |> Seq.map(fun pos -> [1 .. (r-pos |> abs)] |> Seq.sum) |> Seq.sum)  
+|> Seq.min
+|> printfn "%A"
