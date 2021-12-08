8e2aa9d1c9260089d9e7a5fcb87091d8b8065dac
Timo Vahtera <timo.vahtera@solita.fi>
2021-12-08 22:08:08 +0200

    Day 8 formatting

--- day08.fsx
+++ day08.fsx
@@ -18,9 +18,9 @@ let getOutputValue (digits : string array) =
     let eight   = d |> Seq.where (Seq.length >> (=) 7) |> Seq.head
     let three   = d |> Seq.where (Seq.length >> (=) 5) |> Seq.where (Set.isSubset one) |> Seq.head
     let nine    = d |> Seq.where (Seq.length >> (=) 6) |> Seq.where (Set.isSubset three) |> Seq.head
-    let five    = d |> Seq.except [three] |> Seq.where (Seq.length >> (=) 5) |> Seq.where (Set.isSuperset nine) |> Seq.head
-    let two     = d |> Seq.except [five;three] |> Seq.where (Seq.length >> (=) 5) |>  Seq.head
-    let six     = d |> Seq.except [nine] |> Seq.where (Seq.length >> (=) 6) |> Seq.where (Set.isSubset five) |> Seq.head
+    let five    = d |> Seq.where (Seq.length >> (=) 5) |> Seq.except [three]  |> Seq.where (Set.isSuperset nine) |> Seq.head
+    let two     = d |> Seq.where (Seq.length >> (=) 5) |> Seq.except [five;three] |>  Seq.head
+    let six     = d |> Seq.where (Seq.length >> (=) 6) |> Seq.except [nine] |> Seq.where (Set.isSubset five) |> Seq.head
     let zero    = d |> Seq.except [one;two;three;four;five;six;seven;eight;nine] |> Seq.head
     let mapper = Map [ (zero, "0"); (one, "1"); (two, "2"); (three, "3"); (four, "4"); (five, "5"); (six, "6"); (seven, "7"); (eight, "8"); (nine, "9") ]
     digits |> Seq.skip 10 |> Seq.map (fun x -> mapper.[Set x]) |> System.String.Concat |> int
