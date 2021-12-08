eb49c171a832f0b8b99c1a2c13fc4371005c8822
Timo Vahtera <timo.vahtera@solita.fi>
2021-12-08 22:04:29 +0200

    Day 8 cleanup

--- day08.fsx
+++ day08.fsx
@@ -1,7 +1,4 @@
 #time // puzzle: https://adventofcode.com/2021/day/8
-open type System.String
-
-let split (separator:string array) (s:string) = s.Split (separator, System.StringSplitOptions.RemoveEmptyEntries)
 
 // Answer 1
 "inputs/day08.txt" 
@@ -14,19 +11,19 @@ let split (separator:string array) (s:string) = s.Split (separator, System.Strin
 
 // Answer 2
 let getOutputValue (digits : string array) =
-    let d = digits |> Seq.map Seq.sort |> Seq.map Concat |> Seq.distinct
+    let d = digits |> Seq.map Set
     let one     = d |> Seq.where (Seq.length >> (=) 2) |> Seq.head
     let four    = d |> Seq.where (Seq.length >> (=) 4) |> Seq.head
     let seven   = d |> Seq.where (Seq.length >> (=) 3) |> Seq.head
     let eight   = d |> Seq.where (Seq.length >> (=) 7) |> Seq.head
-    let three   = d |> Seq.where (Seq.length >> (=) 5) |> Seq.where (fun s -> one |> Seq.except s |> Seq.isEmpty) |> Seq.head
-    let nine    = d |> Seq.where (Seq.length >> (=) 6) |> Seq.where (fun s -> three |> Seq.except s |> Seq.isEmpty) |> Seq.head
-    let five    = d |> Seq.except [three] |> Seq.where (Seq.length >> (=) 5) |> Seq.where (fun x -> x |> Seq.except nine |> Seq.isEmpty) |> Seq.head
+    let three   = d |> Seq.where (Seq.length >> (=) 5) |> Seq.where (Set.isSubset one) |> Seq.head
+    let nine    = d |> Seq.where (Seq.length >> (=) 6) |> Seq.where (Set.isSubset three) |> Seq.head
+    let five    = d |> Seq.except [three] |> Seq.where (Seq.length >> (=) 5) |> Seq.where (Set.isSuperset nine) |> Seq.head
     let two     = d |> Seq.except [five;three] |> Seq.where (Seq.length >> (=) 5) |>  Seq.head
-    let six     = d |> Seq.except [nine] |> Seq.where (Seq.length >> (=) 6) |> Seq.where (fun x -> five |> Seq.except x |> Seq.isEmpty) |> Seq.head
+    let six     = d |> Seq.except [nine] |> Seq.where (Seq.length >> (=) 6) |> Seq.where (Set.isSubset five) |> Seq.head
     let zero    = d |> Seq.except [one;two;three;four;five;six;seven;eight;nine] |> Seq.head
-    let mapper = Map [ (zero, '0'); (one, '1'); (two, '2'); (three, '3'); (four, '4'); (five, '5'); (six, '6'); (seven, '7'); (eight, '8'); (nine, '9') ]
-    digits |> Seq.skip 10 |> Seq.map (Seq.sort >> Concat >> fun x -> mapper.[x]) |> Concat |> int
+    let mapper = Map [ (zero, "0"); (one, "1"); (two, "2"); (three, "3"); (four, "4"); (five, "5"); (six, "6"); (seven, "7"); (eight, "8"); (nine, "9") ]
+    digits |> Seq.skip 10 |> Seq.map (fun x -> mapper.[Set x]) |> System.String.Concat |> int
 
 "inputs/day08.txt" 
 |> System.IO.File.ReadAllLines 
