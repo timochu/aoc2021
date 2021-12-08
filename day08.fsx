3247ccb0eaa007660b412302421c6bc5c40160aa
Timo Vahtera <timo.vahtera@gmail.com>
2021-12-08 23:09:32 +0200

    Day 8 improve legibility

--- day08.fsx
+++ day08.fsx
@@ -11,15 +11,17 @@
 
 // Answer 2
 let getOutputValue d =
-    let d1 = d |> Seq.find (Seq.length >> (=) 2)
-    let d4 = d |> Seq.find (Seq.length >> (=) 4)
-    let d7 = d |> Seq.find (Seq.length >> (=) 3) 
-    let d8 = d |> Seq.find (Seq.length >> (=) 7)
-    let d3 = d |> Seq.find (fun x -> Seq.length x = 5 && Set.isSubset d1 x)
-    let d9 = d |> Seq.find (fun x -> Seq.length x = 6 && Set.isSubset d3 x)
-    let d5 = d |> Seq.except [d3] |> Seq.find (fun x -> Seq.length x = 5 && Set.isSuperset d9 x)
-    let d2 = d |> Seq.except [d5;d3] |> Seq.find (Seq.length >> (=) 5)
-    let d6 = d |> Seq.except [d9] |> Seq.find (fun x -> Seq.length x = 6 && Set.isSubset d5 x)
+    let (&&.) f g x = f x && g x
+    let length l s = Seq.length s = l
+    let d1 = d |> Seq.find (length 2)
+    let d4 = d |> Seq.find (length 4)
+    let d7 = d |> Seq.find (length 3) 
+    let d8 = d |> Seq.find (length 7)
+    let d3 = d |> Seq.find (length 5 &&. Set.isSubset d1)
+    let d9 = d |> Seq.find (length 6 &&. Set.isSubset d3)
+    let d5 = d |> Seq.except [d3] |> Seq.find (length 5 &&. Set.isSuperset d9)
+    let d2 = d |> Seq.except [d5;d3] |> Seq.find (length 5)
+    let d6 = d |> Seq.except [d9] |> Seq.find (length 6 &&. Set.isSubset d5)
     let d0 = d |> Seq.except [d1;d2;d3;d4;d5;d6;d7;d8;d9] |> Seq.head
     let mapper y = [d0;d1;d2;d3;d4;d5;d6;d7;d8;d9] |> Seq.findIndex ((=) y) |> string
     d |> Seq.skip 10 |> Seq.map mapper |> Seq.reduce (+) |> int
