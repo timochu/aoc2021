4cadd4c0843cf23091e6a9474826ed261ad4c81f
Timo Vahtera <timo.vahtera@solita.fi>
2021-12-08 22:28:16 +0200

    Day 8 cleanup

--- day08.fsx
+++ day08.fsx
@@ -10,24 +10,24 @@
 |> printfn "%A"
 
 // Answer 2
-let getOutputValue (digits : string array) =
-    let d = digits |> Seq.map Set
-    let one     = d |> Seq.where (Seq.length >> (=) 2) |> Seq.head
-    let four    = d |> Seq.where (Seq.length >> (=) 4) |> Seq.head
-    let seven   = d |> Seq.where (Seq.length >> (=) 3) |> Seq.head
-    let eight   = d |> Seq.where (Seq.length >> (=) 7) |> Seq.head
-    let three   = d |> Seq.where (Seq.length >> (=) 5) |> Seq.where (Set.isSubset one) |> Seq.head
-    let nine    = d |> Seq.where (Seq.length >> (=) 6) |> Seq.where (Set.isSubset three) |> Seq.head
-    let five    = d |> Seq.where (Seq.length >> (=) 5) |> Seq.except [three]  |> Seq.where (Set.isSuperset nine) |> Seq.head
-    let two     = d |> Seq.where (Seq.length >> (=) 5) |> Seq.except [five;three] |>  Seq.head
-    let six     = d |> Seq.where (Seq.length >> (=) 6) |> Seq.except [nine] |> Seq.where (Set.isSubset five) |> Seq.head
-    let zero    = d |> Seq.except [one;two;three;four;five;six;seven;eight;nine] |> Seq.head
-    let mapper = Map [ (zero, "0"); (one, "1"); (two, "2"); (three, "3"); (four, "4"); (five, "5"); (six, "6"); (seven, "7"); (eight, "8"); (nine, "9") ]
-    digits |> Seq.skip 10 |> Seq.map (fun x -> mapper.[Set x]) |> System.String.Concat |> int
+let getOutputValue d =
+    let d1 = d |> Seq.where (Seq.length >> (=) 2) |> Seq.head
+    let d4 = d |> Seq.where (Seq.length >> (=) 4) |> Seq.head
+    let d7 = d |> Seq.where (Seq.length >> (=) 3) |> Seq.head
+    let d8 = d |> Seq.where (Seq.length >> (=) 7) |> Seq.head
+    let d3 = d |> Seq.where (Seq.length >> (=) 5) |> Seq.where (Set.isSubset d1) |> Seq.head
+    let d9 = d |> Seq.where (Seq.length >> (=) 6) |> Seq.where (Set.isSubset d3) |> Seq.head
+    let d5 = d |> Seq.where (Seq.length >> (=) 5) |> Seq.except [d3]  |> Seq.where (Set.isSuperset d9) |> Seq.head
+    let d2 = d |> Seq.where (Seq.length >> (=) 5) |> Seq.except [d5;d3] |>  Seq.head
+    let d6 = d |> Seq.where (Seq.length >> (=) 6) |> Seq.except [d9] |> Seq.where (Set.isSubset d5) |> Seq.head
+    let d0 = d |> Seq.except [d1;d2;d3;d4;d5;d6;d7;d8;d9] |> Seq.head
+    let mapper y = [d0;d1;d2;d3;d4;d5;d6;d7;d8;d9] |> Seq.findIndex ((=) y) |> string
+    d |> Seq.skip 10 |> Seq.map mapper |> Seq.reduce (+) |> int
 
 "inputs/day08.txt" 
 |> System.IO.File.ReadAllLines 
 |> Seq.map (fun (s:string) -> s.Split([|" | "; " "|], System.StringSplitOptions.RemoveEmptyEntries))
+|> Seq.map (fun (d : string array) -> d |> Seq.map Set)
 |> Seq.map getOutputValue
 |> Seq.sum
 |> printfn "%A" 
