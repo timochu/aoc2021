24a2ad76fa11b7ebd668cbdca7b4569c30e8e1c3
Timo Vahtera <timo.vahtera@gmail.com>
2021-12-09 20:36:36 +0200

    Day 9 part 2 v1

--- day09.fsx
+++ day09.fsx
@@ -1,24 +1,42 @@
 #time // puzzle: https://adventofcode.com/2021/day/9
+
+let file = "inputs/day09.txt"
+
 let heightmap = 
-    "inputs/day09.txt" 
+    file
     |> System.IO.File.ReadAllLines
     |> Array.map (Seq.map (string >> int) >> Seq.toArray)
     |> Array.concat
+    |> Array.indexed
 
-let width = 100
+let width = file |> System.IO.File.ReadLines |> Seq.head |> Seq.length
 let length = Array.length heightmap
 
 let getAdjacentValues i = 
-    seq { if i % width <> 0 then yield heightmap[i-1] 
-          if i % width <> width - 1 then yield heightmap[i+1]
-          if i - width >= 0 then yield heightmap[i-width]
-          if i + width < length then yield heightmap[i+width] }
+    [| if i % width <> 0 then yield heightmap[i-1]
+       if i % width <> width - 1 then yield heightmap[i+1]
+       if i - width >= 0 then yield heightmap[i-width]
+       if i + width < length then yield heightmap[i+width] |]
 
 let isLowerThanAdjacent (index, value) = 
-    index |> getAdjacentValues |> Seq.min |> (<) value
+    index |> getAdjacentValues |> Seq.minBy snd |> snd |> (<) value
 
-heightmap 
-|> Array.indexed
-|> Array.where isLowerThanAdjacent 
-|> Array.sumBy (snd >> (+) 1 )
+let lowPoints = heightmap |> Array.where isLowerThanAdjacent 
+
+// Answer 1
+lowPoints |> Array.sumBy (snd >> (+) 1) |> printfn "%i"
+
+// Answer 2
+let rec getBasinSize (points : (int*int) array) length : int = // (pointIndex : int, point : int)
+    let adjacent = points |> Array.collect (fun (i,_) -> getAdjacentValues i) |> Array.where (fun (_,p) -> p < 9 ) |> Array.distinctBy fst
+    let basin = adjacent |> Array.append points |> Array.distinctBy fst
+    let newLength = basin.Length
+    if newLength = length then length
+    else getBasinSize basin newLength
+
+lowPoints 
+|> Array.map (fun p -> getBasinSize [|p|] 0) 
+|> Array.sortDescending 
+|> Array.take 3 
+|> Array.reduce (*) 
 |> printfn "%i"
