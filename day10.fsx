1709139c9bd79d8e5f893f27a4bac92a97c55f8e
Timo Vahtera <timo.vahtera@gmail.com>
2021-12-10 22:34:05 +0200

    Day 10 formatting

--- day10.fsx
+++ day10.fsx
@@ -5,24 +5,22 @@ let closings = Set ")]}>"
 let scores1 = Map [(')', 3) ; (']', 57) ; ('}', 1197) ; ('>', 25137)] 
 let scores2 = Map [(')', 1L) ; (']', 2L) ; ('}', 3L) ; ('>', 4L)] 
 
-let getClosing (char : char) = closings |> Seq.item (openings |> Seq.findIndex ((=) char))
+let getClosing char = closings |> Seq.item (openings |> Seq.findIndex ((=) char))
 
 let openingCount = Seq.where openings.Contains >> Seq.length
 
 let closingCount = Seq.where closings.Contains >> Seq.length
 
-let isCompleteChunk x = openingCount x = closingCount x
+let isCompleteChunk chunk = openingCount chunk = closingCount chunk
 
-let hasMismatchingBrackets x =
-    let firstChar = List.head x
-    let lastChar = List.last x
-    let openingIndex = openings |> Seq.findIndex ((=) firstChar)
-    let closingIndex = closings |> Seq.findIndex ((=) lastChar)
-    openingIndex <> closingIndex
+let hasMismatchingBrackets chunk =
+    ( openings |> Seq.findIndex ((=) (List.head chunk)),
+      closings |> Seq.findIndex ((=) (List.last chunk)) ) 
+      ||> (<>)
 
 let isCorrupt x = isCompleteChunk x && hasMismatchingBrackets x
 
-let chunkify (x : char list) =
+let toChunks line =
     let rec chunk (chunks : char list list) (aggregate : char list) (line : char list) =
         if  openingCount line = 0 then chunks
         else
@@ -34,22 +32,19 @@ let chunkify (x : char list) =
             | true -> 
                 let startFrom = line.Tail |> List.skipWhile closings.Contains
                 if startFrom.IsEmpty then newAggregate :: chunks
-                else chunk (newAggregate :: chunks) [startFrom.Head] (startFrom)
+                else chunk (newAggregate::chunks) [startFrom.Head] startFrom
             | false ->
                 chunk chunks newAggregate line
-    chunk [] [x.Head] x
+    chunk [] [Seq.head line] (line |> Seq.toList)
 
-let autocompletePoints (chunks : char list list) =
+let calculateAutocompleteScore chunks =
     chunks 
     |> List.where (isCompleteChunk >> not) 
     |> List.map (List.head >> getClosing)
     |> List.map (fun x -> scores2.Item x)
     |> List.fold (fun acc s -> (acc * 5L) + s) 0L
 
-let chunks = 
-    "inputs/day10test.txt" 
-    |> System.IO.File.ReadAllLines 
-    |> Array.map (Seq.toList >> chunkify)
+let chunks = "inputs/day10test.txt" |> System.IO.File.ReadAllLines |> Array.map toChunks
 
 // // Answer 1
 chunks
@@ -63,7 +58,7 @@ chunks
 // // Answer 2
 chunks
 |> Array.where (List.exists isCorrupt >> not)
-|> Array.map autocompletePoints
+|> Array.map calculateAutocompleteScore
 |> Array.sort
 |> fun scores -> scores[scores.Length / 2]
 |> printfn "%A"
