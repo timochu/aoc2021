8b10eeca89f2ed283cb29ac4b0b7efb24efa8e86
Timo Vahtera <timo.vahtera@gmail.com>
2021-12-10 22:02:17 +0200

    Day 10 part 2

--- day10.fsx
+++ day10.fsx
@@ -2,9 +2,10 @@
 
 let openings = Set "([{<"
 let closings = Set ")]}>"
-let scoreMap = Map [(')', 3) ; (']', 57) ; ('}', 1197) ; ('>', 25137)]
+let scores1 = Map [(')', 3) ; (']', 57) ; ('}', 1197) ; ('>', 25137)] 
+let scores2 = Map [(')', 1L) ; (']', 2L) ; ('}', 3L) ; ('>', 4L)] 
 
-let score x = scoreMap.Item x
+let getClosing (char : char) = closings |> Seq.item (openings |> Seq.findIndex ((=) char))
 
 let openingCount = Seq.where openings.Contains >> Seq.length
 
@@ -32,19 +33,36 @@ let chunkify (x : char list) =
             match aggregate.Length = newAggregate.Length || isCompleteChunk newAggregate with
             | true -> 
                 let startFrom = line.Tail |> List.skipWhile closings.Contains
-                if startFrom.IsEmpty then aggregate :: chunks
+                if startFrom.IsEmpty then newAggregate :: chunks
                 else chunk (newAggregate :: chunks) [startFrom.Head] (startFrom)
             | false ->
                 chunk chunks newAggregate line
     chunk [] [x.Head] x
 
+let autocompletePoints (chunks : char list list) =
+    chunks 
+    |> List.where (isCompleteChunk >> not) 
+    |> List.map (List.head >> getClosing)
+    |> List.map (fun x -> scores2.Item x)
+    |> List.fold (fun acc s -> (acc * 5L) + s) 0L
+
 // // Answer 1
-"inputs/day10.txt" 
+"inputs/day10test.txt" 
 |> System.IO.File.ReadAllLines 
 |> Array.map (Seq.toList >> chunkify)
 |> Array.where (List.exists isCorrupt)
 |> Array.map (List.where isCorrupt >> List.map List.last)
 |> List.concat
-|> List.map score
+|> List.map (fun x -> scores1.Item x)
 |> List.sum
 |> printfn "%i"
+
+// // Answer 2
+"inputs/day10.txt" 
+|> System.IO.File.ReadAllLines 
+|> Array.map (Seq.toList >> chunkify)
+|> Array.where (List.exists isCorrupt >> not)
+|> Array.map autocompletePoints
+|> Array.sort
+|> fun x -> x[x.Length/2]
+|> printfn "%A"
